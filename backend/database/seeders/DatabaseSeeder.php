aed374a9f3fda55a36f6e2a5b71b1e49755cb0a1
Soukaina Arjdali <soukaina.arjdali@gmail.com>
2024-11-05 22:45:36 +0100

    ajouter ReviewSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 use Database\Seeders\CategorieSeeder;
 use Database\Seeders\JewelrySeeder;
 use Database\Seeders\CustomerSeeder;
+use Database\Seeders\ReviewSeeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
             CategorieSeeder::class,
             JewelrySeeder::class,
             CustomerSeeder::class,
+            ReviewSeeder::class,
         ]);
 
     }
