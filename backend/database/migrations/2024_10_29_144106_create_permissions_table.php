e14224a88d070eea63570bf093af0ef78a3e9bac
Soukaina Arjdali <soukaina.arjdali@gmail.com>
2024-11-06 01:33:31 +0100

    petite modification

--- backend/database/migrations/2024_10_29_144106_create_permissions_table.php
+++ backend/database/migrations/2024_10_29_144106_create_permissions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('desription');
+            $table->string('description');
             $table->timestamps();
         });
     }
