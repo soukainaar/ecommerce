2a6bacea230f60bad9dd3d3b3ecc5d1e09170e50
Soukaina Arjdali <soukaina.arjdali@gmail.com>
2024-11-06 01:31:00 +0100

    ajouter les seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ use Database\Seeders\CategorieSeeder;
 use Database\Seeders\JewelrySeeder;
 use Database\Seeders\CustomerSeeder;
 use Database\Seeders\ReviewSeeder;
+use Database\Seeders\OrderSeeder;
+use Database\Seeders\PermissionSeeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,6 +23,8 @@ class DatabaseSeeder extends Seeder
             JewelrySeeder::class,
             CustomerSeeder::class,
             ReviewSeeder::class,
+            OrderSeeder::class,
+            PermissionSeeder::class,
         ]);
 
     }
