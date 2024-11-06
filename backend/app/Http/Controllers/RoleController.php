723a6c9892c5fbd10ba0cee35faca95f5ac14843
Soukaina Arjdali <soukaina.arjdali@gmail.com>
2024-11-07 00:37:36 +0100

    remplir les fonctions de la table role

---
+++ backend/app/Http/Controllers/RoleController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles = Role::all();
+        return response()->json($roles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+        $role = Role::create($validateData);
+        return response()->json($role, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $role = Role::find($id);
+        return response()->json($role);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+        $role = Role::find($id);
+        $role->update($validateData);
+        return response()->json($role, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $role = Role::find($id);
+        $role->delete();
+        return response()->json(null, 204);
+    }
+}
