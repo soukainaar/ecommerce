0ef146c8f33ba22b51459a6fda51506f43458ef3
Soukaina Arjdali <soukaina.arjdali@gmail.com>
2024-11-06 22:07:07 +0100

    remplir les fonctions de categories

---
+++ backend/app/Http/Controllers/CategorieController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categorie;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Categorie::all();
+        return response()->json($categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required'
+        ]);
+        $categorie = Categorie::create($validateData);
+        return response()->json($categorie, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $categorie = Categorie::find($id);
+        return response()->json($categorie);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required'
+        ]);
+        $categorie = Categorie::find($id);
+        $categorie->update($validateData);
+        return response()->json($categorie, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $categorie = Categorie::find($id);
+        $categorie->delete();
+        return response()->json(null, 204);
+    }
+}
