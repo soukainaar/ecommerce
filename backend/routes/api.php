50fd04f5b94b4761498041e22ce8baaac3a1cf6c
Soukaina Arjdali <soukaina.arjdali@gmail.com>
2024-11-07 00:35:56 +0100

    ajouter le reste des routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JewelryController;
 use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +27,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('jewelries', JewelryController::class);
 Route::apiResource('categories', CategorieController::class);
+Route::apiResource('reviews', ReviewController::class);
+Route::apiResource('customers', CustomerController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('roles', RoleController::class);
